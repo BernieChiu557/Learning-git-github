e74812ddae882226166dbc1be9d155865d233dc9
Yi-Hung-Chiu <bernie55764@gmail.com>
2022-11-19 13:32:48 -0500

    add function to say hi to family

--- hello.py
+++ hello.py
@@ -3,8 +3,13 @@ import argparse
 def hello_name(name):
     print(f'hello {name}')
 
+def hello_to_family(name_list):
+    speaker = name_list[0]
+    for i in range(len(name_list)-1):
+        print(f'{speaker} say hi to {name_list[i+1]}')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--name', type=str, default='Bob')
+    parser.add_argument('-ns', '--names',nargs='+', type=str, default='Bob')
     args = parser.parse_args()
-    hello_name(args.name)
+    hello_to_family(args.names)
